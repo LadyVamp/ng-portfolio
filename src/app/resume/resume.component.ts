8869ea55ca0a83872c5674abadaacf94b132b147
LadyVamp <ladyvamp666@gmail.com>
2022-01-05 18:26:49 +0300

    easter egg

--- src/app/resume/resume.component.ts
+++ src/app/resume/resume.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from '../../../package.json';
 
 @Component({
   selector: 'app-resume',
@@ -6,9 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resume.component.scss']
 })
 export class ResumeComponent implements OnInit {
+  counter = 0;
+  version: string = version;
+
+  rsbImages = [
+    { url: './assets/img/resume/rsb/email1.png', web: 'https://www.rsb.ru/f/1/global/i/CRM/rsb/03102019/PIL_web.html', title: 'Кредит наличными на особых условиях' },
+    { url: './assets/img/resume/rsb/email2.png', web: 'https://www.rsb.ru/f/1/global/i/CRM/rsb/01062019_DJ/3082.html', title: 'Дайджест июнь' },
+    { url: './assets/img/resume/rsb/check_banner1.jpg', title: 'Баннер 1' },
+    { url: './assets/img/resume/rsb/check_banner2.png', title: 'Баннер 2' },
+    { url: './assets/img/resume/rsb/claim1.jpg', title: 'Форма для исключения клиентов' },
+    { url: './assets/img/resume/rsb/claim2.jpg', title: 'Форма для исключения клиентов / пример валидации' },
+    { url: './assets/img/resume/rsb/faceident1.png', title: 'Сервис распознавания лиц' },
+    { url: './assets/img/resume/rsb/faceident2.png', title: 'Сервис распознавания лиц / пример результата поиска' },
+    { url: './assets/img/resume/rsb/cashoff1.png', title: 'Cashoff / скриншоты макетов' },
+    { url: './assets/img/resume/rsb/cashoff2.png', title: 'Cashoff / скриншоты приложения' },
+    { url: './assets/img/resume/rsb/dialer_ng_client.png', title: 'Dialer CRM / карточка клиента' },
+    { url: './assets/img/resume/rsb/dialer_ng_survey.png', title: 'Dialer CRM / окно назначения встречи' },
+    { url: './assets/img/resume/rsb/camp_home.png', title: 'CRM Camp / главная страница' },
+    { url: './assets/img/resume/rsb/camp_edit.png', title: 'CRM Camp / редактирование кампании' },
+  ]
+
+  vskImages = [
+    { url: './assets/img/resume/vsk/smev.png', title: 'СМЭВ' },
+    { url: './assets/img/resume/vsk/sunp1.png', title: 'СУНП / конфигурации' },
+    { url: './assets/img/resume/vsk/sunp2.png', title: 'СУНП / выбрана конфигурация' },
+    { url: './assets/img/resume/vsk/sunp3.png', title: 'СУНП / редактирование конфигурации' },
+    { url: './assets/img/resume/vsk/sunp4.png', title: 'СУНП / запросы на применение' },
+    { url: './assets/img/resume/vsk/guard1.jpg', title: 'Росгвардия / меню' },
+    { url: './assets/img/resume/vsk/guard2.jpg', title: 'Росгвардия / сервисы' },
+    { url: './assets/img/resume/vsk/guard3.jpg', title: 'Росгвардия / фильтры' },
+    { url: './assets/img/resume/vsk/gas1.png', title: 'ГАС / реестр операторов' },
+    { url: './assets/img/resume/vsk/gas2.png', title: 'ГАС / редактирование оператора' },
+    { url: './assets/img/resume/vsk/gas3.png', title: 'ГАС / редактирование публикации' },
+    { url: './assets/img/resume/vsk/dashboard.png', title: 'СЦ дашборд' },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  increment() {
+    this.counter++;
   }
 
 }
