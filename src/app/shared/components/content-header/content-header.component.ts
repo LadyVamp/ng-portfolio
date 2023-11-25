79fa2dd282c99d4af47a8e0f09e4821d44e62d59
LadyVamp <ladyvamp666@gmail.com>
2023-11-25 18:31:56 +0300

    fix surname

--- src/app/shared/components/content-header/content-header.component.ts
+++ src/app/shared/components/content-header/content-header.component.ts
@@ -5,7 +5,7 @@ import 'dayjs/locale/ru';
 @Component({
   selector: 'app-content-header',
   templateUrl: './content-header.component.html',
-  styleUrls: ['./content-header.component.scss']
+  styleUrls: ['./content-header.component.scss'],
 })
 export class ContentHeaderComponent implements OnInit {
   @Input() dates: string[];
@@ -13,34 +13,43 @@ export class ContentHeaderComponent implements OnInit {
   @Input() subtitle: string;
   @Input() logo: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   formatDates(start: string, end: string): string {
     dayjs.locale('ru');
 
     if (end === 'now') {
-      return (dayjs(start).format('MMMM YYYY') + ' — по настоящее время');
+      return dayjs(start).format('MMMM YYYY') + ' — по настоящее время';
     } else {
-      return (dayjs(start).format('MMMM YYYY') + ' — ' + dayjs(end).format('MMMM YYYY'));
+      return (
+        dayjs(start).format('MMMM YYYY') +
+        ' — ' +
+        dayjs(end).format('MMMM YYYY')
+      );
     }
   }
 
   differenceBetweenDates(start: string, end: string): string {
     let endDate;
     const startDate = dayjs(start);
-    end === 'now' ? endDate = dayjs() : endDate = dayjs(end);
+    end === 'now' ? (endDate = dayjs()) : (endDate = dayjs(end));
     const diff = endDate.diff(startDate, 'month');
     const y = Math.floor(diff / 12);
     const m = diff % 12;
     if (y === 0) {
       return m + this.monthEnding(m);
+    } else if (m === 0 && y >= 2 && y <= 4) {
+      return y + ' года';
+    } else if (m === 0 && y >= 5) {
+      return y + ' лет';
+    } else if (m !== 0 && y >= 5) {
+      return y + ' лет ' + m + this.monthEnding(m);
     } else if (m === 0) {
-      return (y + ' год');
+      return y + ' год';
     } else {
-      return (y + ' год ' + m + this.monthEnding(m));
+      return y + ' год ' + m + this.monthEnding(m);
     }
   }
 
@@ -52,5 +61,4 @@ export class ContentHeaderComponent implements OnInit {
       return ' месяцев';
     }
   }
-
 }
